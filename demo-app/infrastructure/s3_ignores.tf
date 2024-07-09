07edb8fc661f38e4d596e403a8bba05f4e9b263e
Alan Nix <alanneilnix@gmail.com>
2024-07-09 15:17:58 -0400

    feat: adding an awesome Python application

---
+++ demo-app/infrastructure/s3_ignores.tf
@@ -0,0 +1,43 @@
+# wiz-scan ignore
+resource "aws_s3_bucket" "mongodb_backups" {
+  bucket        = "wiz-demo-mongodb-backups-ignore"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "mongodb_backups" {
+  bucket = aws_s3_bucket.mongodb_backups.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# wiz-scan ignore-block
+resource "aws_s3_bucket_acl" "mongodb_backups" {
+  bucket = aws_s3_bucket.mongodb_backups.id
+  # wiz-scan ignore-line
+  acl = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "allow_public_access" {
+  bucket = aws_s3_bucket.mongodb_backups.id
+  policy = data.aws_iam_policy_document.allow_public_access.json
+}
+
+data "aws_iam_policy_document" "allow_public_access" {
+  statement {
+    principals {
+      identifiers = ["*"]
+      type        = "*"
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.mongodb_backups.arn}/*"
+    ]
+  }
+}
