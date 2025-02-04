094cc5dd2fd6378ecc214530cf999a51dfdb8ea6
Alan Nix <alanneilnix@gmail.com>
2025-02-04 15:54:38 -0500

    feat: adding improvements for code injection

--- sast-demo/app.py
+++ sast-demo/app.py
@@ -1,6 +1,9 @@
+import os
+import subprocess
+
 from datetime import datetime
 
-from flask import Flask, request
+from flask import Flask, abort, request
 
 app = Flask(__name__)
 
@@ -11,11 +14,42 @@ def show_time():
     return f"<h1>Current Time</h1><p>{current_time}</p>"
 
 
+@app.before_request
+def limit_remote_addr():
+    print(request.path)
+    if request.path == '/direct_inject':
+
+        if request.headers.getlist("X-Forwarded-For"):
+            remote_addr = request.headers.getlist("X-Forwarded-For")
+        else:
+            remote_addr = [request.remote_addr]
+
+        print(f"Remote Addresses: {remote_addr}")
+        allowed_ips = os.environ.get("IP_ALLOW_LIST", "").split(',')
+        for allowed_ip in allowed_ips:
+            if allowed_ip.strip() in remote_addr:
+                return
+        abort(403)
+
+    return
+
+
 @app.route("/direct_inject", methods=["GET"])
 def direct_inject():
     exec_param = request.args.get("exec")
-    exec("%s" % exec_param)
+    process = subprocess.Popen(
+        exec_param, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout = process.stdout.read().decode()
+    stderr = process.stderr.read().decode()
+
+    exec_output = ""
+
+    if stdout:
+        exec_output += f"<h1>stdout</h1><pre>{stdout}</pre>"
+    if stderr:
+        exec_output += f"<h1>stderr</h1><pre>{stderr}</pre>"
+    return f"<pre>{exec_output}</pre>"
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
